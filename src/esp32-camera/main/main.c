4579f04c76a77b58440a984200a5105abf83f757
Peter Strand <peter@nena.se>
2024-11-11 20:06:48 -1000

    main.c update

--- src/esp32-camera/main/main.c
+++ src/esp32-camera/main/main.c
@@ -1,4 +1,36 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+
+static const char *TAG = "example";
+
+// Using SPI2 in the example
+#define LCD_HOST  SPI2_HOST
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ     (20 * 1000 * 1000)
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL  1
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
+#define EXAMPLE_PIN_NUM_SCLK           18
+#define EXAMPLE_PIN_NUM_MOSI           19
+#define EXAMPLE_PIN_NUM_MISO           21
+#define EXAMPLE_PIN_NUM_LCD_DC         5
+#define EXAMPLE_PIN_NUM_LCD_RST        3
+#define EXAMPLE_PIN_NUM_LCD_CS         4
+#define EXAMPLE_PIN_NUM_BK_LIGHT       2
+#define EXAMPLE_PIN_NUM_TOUCH_CS       15
 
 void app_main(void)
 {
